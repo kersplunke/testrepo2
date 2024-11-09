bf28dc2657c0c1f297d8bb3708b216be7d0750ef
kersplunke <kchapple@sevenpeaksschool.org>
2024-11-08 16:40:26 -0800

    Firt commit

---
+++ testImportJson.js
@@ -0,0 +1,18 @@
+function logit(output){
+    output.forEach(element => {
+        console.log(element)
+    })
+}
+
+
+function getDataFromURL (){
+    const url = 'https://script.google.com/macros/s/AKfycbwlxYDnTQbuIyqNfXxWeiyIohzZPRH5GpfZmmMgwl3lLa01gERR59d6tWC7yBoSI_1f/exec'
+    return fetch(url)
+        .then(res => res.json())
+        .then(out => logit(out))
+        .catch(err => console.log(err)); 
+}
+
+
+getDataFromURL()
+
